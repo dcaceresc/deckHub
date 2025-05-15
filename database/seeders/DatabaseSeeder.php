9d005ac101c4205f79a8487ea45656535daf7045
Diego Cáceres <mavroslykanthropos@outlook.com>
2025-05-14 23:34:02 -0400

    Se modifico readme y se reparo errores

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call([
+            UserSeeder::class,
             GameSeeder::class,
             CardTypeSeeder::class,
             CardSeeder::class,
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            ['name' => 'admin', 'email' => 'admin@admin.com', 'password' => bcrypt('password'), 'created_at' => now(), 'updated_at' => now()]
+        ]);
+    }
+}
